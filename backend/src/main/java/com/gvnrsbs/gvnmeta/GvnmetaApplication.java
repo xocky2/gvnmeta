3d96b9083cd909e122841ee40083bf759e74c1e5
Giovanni Barros <56982689+xocky2@users.noreply.github.com>
2022-09-05 11:18:17 -0300

    Project created

---
+++ backend/src/main/java/com/gvnrsbs/gvnmeta/GvnmetaApplication.java
@@ -0,0 +1,13 @@
+package com.gvnrsbs.gvnmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GvnmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GvnmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gvnrsbs/gvnmeta/GvnmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gvnrsbs.gvnmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GvnmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
