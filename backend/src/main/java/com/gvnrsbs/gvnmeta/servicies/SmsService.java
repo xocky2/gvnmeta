4daa9a68d2d6928295b190ecd42292fd5bf98f46
Giovanni Barros <56982689+xocky2@users.noreply.github.com>
2022-09-10 13:12:11 -0300

    Simple SMS message

--- backend/src/main/java/com/gvnrsbs/gvnmeta/servicies/SmsService.java
+++ backend/src/main/java/com/gvnrsbs/gvnmeta/servicies/SmsService.java
@@ -34,7 +34,7 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", sale.getSellerName(), date, sale.getAmount());
+		String msg = ("Teste de mensagem sem nenhuma concatenação ");
 
 		Twilio.init(twilioSid, twilioKey);
 
